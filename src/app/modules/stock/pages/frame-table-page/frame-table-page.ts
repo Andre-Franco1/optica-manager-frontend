d9c86f0e219c53170c6149b80b7a3bb8f925e32a
Andre-Franco1 <andrefranco3172@gmail.com>
2026-02-24 13:48:44 -0300

    feat: create increase and decrease stock buttons in frame table page

--- src/app/modules/stock/pages/frame-table-page/frame-table-page.ts
+++ src/app/modules/stock/pages/frame-table-page/frame-table-page.ts
@@ -9,17 +9,23 @@ import { RouterLink } from '@angular/router';
 import { ModalComponent } from '../../../../shared/components/modal/modal';
 import { FrameBrandLabels } from '../../../../core/enums/frame-brand';
 import { FrameTypeLabels } from '../../../../core/enums/frame-type';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { StockService } from '../../../../core/services/stock';
+import { StockMovementType } from '../../../../core/enums/stock-movement-type';
+import { StockRequest } from '../../../../core/models/stock-request';
 
 @Component({
   selector: 'app-frame-table-page',
-  imports: [FormsModule, NgbPaginationModule, RouterLink, ModalComponent],
+  imports: [FormsModule, NgbPaginationModule, RouterLink, ModalComponent, ReactiveFormsModule],
   templateUrl: './frame-table-page.html',
   styleUrl: './frame-table-page.css',
 })
 export class FrameTablePageComponent implements OnInit {
 
   frameService = inject(FrameService);
+  stockService = inject(StockService);
   toastService = inject(ToastService);
+  formBuilder = inject(FormBuilder);
 
   framePage: Page<Frame> = {} as Page<Frame>;
   page = 1;
@@ -29,6 +35,16 @@ export class FrameTablePageComponent implements OnInit {
 
   nameFilter: string = "";
   selectedFrame !: Frame;
+
+  stockMovementTypes = StockMovementType;
+  
+  modalTitle = '';
+  selectedStockMovementType!: StockMovementType;
+
+  stockForm: FormGroup = this.formBuilder.group({
+    quantity: ['', [Validators.required, Validators.min(1)]],
+    comment: ['']
+  });
 
   ngOnInit(): void {
     this.loadFrames();
@@ -65,6 +81,51 @@ export class FrameTablePageComponent implements OnInit {
         });
       }
     })
-
   }
+
+  stock(frame: Frame, type: StockMovementType, modal: ModalComponent) {
+
+  this.selectedFrame = frame;
+  this.selectedStockMovementType = type;
+
+  this.modalTitle =
+    type === this.stockMovementTypes.ENTRY
+      ? 'Adicionar Estoque'
+      : 'Remover Estoque';
+
+  this.stockForm.reset();
+
+  modal.open().then(confirm => {
+
+    if (!confirm) return;
+
+    if (this.stockForm.invalid || !this.selectedFrame) {
+      this.stockForm.markAllAsTouched();
+      return;
+    }
+
+    const { quantity, comment } = this.stockForm.getRawValue();
+
+    const payload: StockRequest = {
+      quantity: Number(quantity),
+      comment: comment || undefined
+    };
+
+    const action$ =
+      this.selectedStockMovementType === this.stockMovementTypes.ENTRY
+        ? this.stockService.increaseStock(this.selectedFrame.id, payload)
+        : this.stockService.decreaseStock(this.selectedFrame.id, payload);
+
+    action$.subscribe({
+      next: () => {
+        this.toastService.show("Estoque atualizado com sucesso!", "bg-success text-light");
+        this.loadFrames();
+      },
+      error: () => {
+        this.toastService.show('Houve um erro ao atualizar o estoque', 'bg-danger text-light')
+      }
+    });
+
+  });
+}
 }
